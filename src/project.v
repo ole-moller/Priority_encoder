660e3dce3e83d5a2665cc52d5150ea0e8d9fcbed
ole-moller <140734002+ole-moller@users.noreply.github.com>
2025-02-21 18:46:57 +0100

    Update project.v
    Using Verilog assign statements.

--- src/project.v
+++ src/project.v
@@ -1,20 +1,21 @@
-// Find index of most significant data bit set to 1 among 8 data bits, and output result to 7-segment display.
+// 8-bit priority encoder with 7-segment decoder.
+// Find index of most significant input data bit set to 1, and output result as 7-segment code (segments abcdefg).
 // Binary data bit 7 has highest priority, bit 0 lowest.
-// If no data bits are set then all 7-segments are off, but decimal point (output none) is on.
+// If no data bits are set then all 7-segments are off, but output pin None (decimal point dp) is on instead.
 // Implemented with 8-bit priority encoder followed by 3-bit to 7-segment decoder.
 
-module tt_um_Priority_encoder_to_7_segment_display (
-    input  logic data[7:0],
-    output logic segments[6:0],
-    output logic none);
+module tt_um_Priority_encoder_to_7_segment_display (data, segments, none);
+    input  [7:0] data,
+    output [6:0] segments,
+    output none;
 
-    // Interface between priority encoder and octal-to-7-segment driver
+    // Interface between priority encoder and 7-segment decoder
 
-    logic [2:0] code;
+    wire [2:0] code;
 
-    // 7-segment encoding of digits
+    // 7-segment decoding of digits
 
-    logic [6:0] digit [8] = '{
+    wire [6:0] digit [2:0] = '{
     //     gfedcba
 	7'b0111111, // zero
 	7'b0000110, // one
@@ -28,18 +29,13 @@ module tt_um_Priority_encoder_to_7_segment_display (
 
     // Priority encoding
 
-    code[2] = data[7] | data[6] | data[5] | data[4];
-    code[1] = data[7] | data[6] | ~data[5] & ~data[4] & (data[3] | data[2]);
-    code[0] = data[7] | ~data[6] & (data[5] | ~data[4] & (data[3] | ~data[2] & data[1]));
+    assign code[2] = data[7] | data[6] | data[5] | data[4];
+    assign code[1] = data[7] | data[6] | ~data[5] & ~data[4] & (data[3] | data[2]);
+    assign code[0] = data[7] | ~data[6] & (data[5] | ~data[4] & (data[3] | ~data[2] & data[1]));
 
     // Output to 7-segment display with decimal point that indicates no data
 
-    always_comb
-	begin
-	none = ~data[7] & ~data[6] & ~data[5] & ~data[4] & ~data[3] & ~data[2] & ~data[1] & ~data[0];
-	if (none) begin	segments = 7'b0000000;
-	    end else begin
-	segments = digit[code]; end;
-	end
+    assign none = ~data[7] & ~data[6] & ~data[5] & ~data[4] & ~data[3] & ~data[2] & ~data[1] & ~data[0];
+    assign segments = (none) ? 7'b0000000 : digit[code];
 
 endmodule
