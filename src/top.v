bcc8b5c899cf52df9d02bc1f5d15cd23c8bd87bc
ole-moller <140734002+ole-moller@users.noreply.github.com>
2025-03-08 18:26:35 +0100

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Ole Henrik Møller
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `define default_netname none
 
-module tt_um_signed_unsigned_4x4_bit_multiplier(
+module tt_um_Ole_Henrik_Moller_Priority encoder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,11 @@ module tt_um_signed_unsigned_4x4_bit_multiplier(
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+
   assign uio_out = 0;
   assign uio_oe  = 0;
   
-  // Insert multiplier logic here 
+  // Insert Priority encoder logic here 
   signed_unsigned_4x4_bit_multiplier mult( 
     .multiplicand(ui_in[7:4]),
     .multiplier(ui_in[3:0]),
