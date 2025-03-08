d484ff79239221f829e7ed07c0bfabdb0c4000cb
ole-moller <140734002+ole-moller@users.noreply.github.com>
2025-03-08 21:37:51 +0100

    Update priority_encoder_to_7_segment_decoder.v

--- src/priority_encoder_to_7_segment_decoder.v
+++ src/priority_encoder_to_7_segment_decoder.v
@@ -11,30 +11,9 @@ module priority_encoder_to_7_segment_decoder (
     output wire [6:0] digit,	// Dedicated outputs
     output wire no_data); 	// Dedicated output
 
-//module tt_um_ole_moller_priority_encoder_to_7_segment_decoder (
-//    input  wire [7:0] ui_in,    // Dedicated inputs
-//    output wire [7:0] uo_out,   // Dedicated outputs
-//    input  wire [7:0] uio_in,   // IOs: Input path
-//    output wire [7:0] uio_out,  // IOs: Output path
-//    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-//    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-//    input  wire       clk,      // clock
-//    input  wire       rst_n);   // reset_n - low to reset
-
-//    // Declare internal signals
-
-//    wire [7:0] data;		// Input to priority encoder
-//    wire [2:0] code;		// Interface between priority encoder and 7-segment decoder
-//    wire [6:0] digit [7:0];	// Output to 7-segment display
-//    wire no_data;		// Output to decimal point of 7-segment display
-
-//    // Connect template signals to internal signals
-
-//    assign data = ui_in;
-//    assign uo_out[6:0] = digit;
-//    assign uo_out[7] = none;
-//    assign uio_oe = 0;
-//    assign uio_out = 0;
+    // Interface between priority encoder and 7-segment decoder
+	
+    wire [2:0] code;
 
     // 7-segment decoding of digits
 
