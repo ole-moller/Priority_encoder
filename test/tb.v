ec678a1caed6440f1b787021852a5e430215dbbb
ole-moller <140734002+ole-moller@users.noreply.github.com>
2025-03-11 21:37:22 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_ole_moller_priority_encoder_to_7_segment_decoder (
+  tt_um_ole_moller_priority_encoder_to_7_segment_decoder dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
